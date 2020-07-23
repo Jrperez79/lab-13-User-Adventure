609e31a5f3d4bbc1fe654a9d6134b7f4b01cfd50
Jrperez79 <joelrperez@gmail.com>
2020-07-23 15:15:43 -0700

    updated quest folder and TDD passing

--- quest/quest.js
+++ quest/quest.js
@@ -1,1 +1,14 @@
-console.log('this is working');
+import allQuests from '../data.js';
+import { findById, renderSection } from './questUtils.js';
+
+
+const main = document.querySelector('main');
+
+const questId = 'monsters';
+
+const questData = findById(allQuests, questId);
+
+const sectionEl = renderSection(questData);
+
+main.append(sectionEl);
+
